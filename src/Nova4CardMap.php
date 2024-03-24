d838d9006cd7d61f5fb90b22d641b93bdf5ad0a1
Chad Copeland <imumz@icloud.com>
2024-03-24 17:00:35 +0200

    initial push

--- src/Nova4CardMap.php
+++ src/Nova4CardMap.php
@@ -22,4 +22,60 @@ class Nova4CardMap extends Card
     {
         return 'nova-4-card-map';
     }
+
+    public function height($height){
+        return $this->withMeta([
+            'height' => $height
+        ]);
+    }
+    public function zoom($zoom){
+        return $this->withMeta([
+            'zoom' => $zoom
+        ]);
+    }
+    public function geoJson($geoJson){
+
+        return $this->withMeta([
+            'geoJson' => $geoJson
+        ]);
+    }
+    public function mapIcon($mapIcon,$mapIconSize = null,$mapIconAnchor = null){
+
+        return $this->withMeta([
+            'mapIcon' => $mapIcon,
+            'mapIconSize' => $mapIconSize,
+            'mapIconAnchor' => $mapIconAnchor
+        ]);
+    }
+
+    public function type($type){
+        return $this->withMeta([
+            'type' => $type
+        ]);
+    }
+    public function popup($popup){
+        return $this->withMeta([
+            'popup' => $popup
+        ]);
+    }
+    public function point($latitude,$longitude) {
+        return $this->withMeta([
+            'latitude' => $latitude,
+            'longitude' => $longitude
+        ]);
+    }
+
+    public function googleApiKey($googleApiKey) {
+
+        return $this->withMeta([
+            'googleApiKey' => $googleApiKey
+        ]);
+    }
+
+    public function googleMapType($googleMapType) {
+
+        return $this->withMeta([
+            'googleMapType' => $googleMapType
+        ]);
+    }
 }
